d2e8b9eccc69320c64eea21ee0ac2c60e7431a72
Nikolai Zakharov <nickolai.zakharov.2020@gmail.com>
2020-10-20 23:14:22 +0300

    v.0.5.4.1 Add forgotten database_init file

---
+++ store/database_init.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from store.store_api import StoreApi
+
+if __name__ == "__main__":
+    api = StoreApi()
+
+    # Inserting book page validation regexps
+    api.insert_platform_book_validation_regexp({
+        'platform': 'litmarket.ru',
+        'regexp': 'div class="book-page"',
+    })
+
+    # Inserting representations.
