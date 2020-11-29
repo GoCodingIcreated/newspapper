726eb0f879d2c52a0cc7fc088b45918eee1c1d09
Nikolai Zakharov <nickolai.zakharov.2020@gmail.com>
2020-11-29 17:16:06 +0300

    v.0.5.12. Fix minor bugs in status script

--- alert/telegram/status.sh
+++ alert/telegram/status.sh
@@ -7,12 +7,12 @@ notify_file=$DIRNAME/../../logs/newspapper_bot.service.notifed
 if [ -z "$(systemctl status newspapper_bot.service  | grep "active (running)")" ]; then
     if [ ! -f $notify_file ]; then
         $DIRNAME/send_alarm.sh "NewspapperBot service is not running"
+        touch $notify_file
     fi
-	touch $notify_file
 else
 	if [ -f $notify_file ]; then
         $DIRNAME/send_alarm.sh "NewspapperBot service is running"
+        rm -f $notify_file
     fi
-	rm -f $notify_file
 fi
 
